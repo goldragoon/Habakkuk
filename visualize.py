b9e769b2c3a55d0e5343a7b313220190bce2589a
paganinist <paganinist@gmail.com>
2018-01-31 10:48:18 +0900

    Fix Heatmap Visualization Error

--- visualize.py
+++ visualize.py
@@ -7,8 +7,8 @@ import seaborn as sns
 
 
 def drawHeatMap(indexes, cols, data):
-    print(indexes, cols)
-    df = DataFrame(abs(np.random.randn(len(indexes), len(cols))), index=indexes, columns=cols)
+    df = DataFrame(data, index=indexes, columns=cols)
     heatmap = sns.heatmap(df, annot=True)
     plt.savefig("performance_heatmap.png")
+
 
